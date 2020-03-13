892ed124cdee242c7cfdf0c45ad48fc912721e4c
Cauany Souza <souza.cauany99@gmail.com>
2020-03-13 11:59:56 -0300

    Configurando rotas

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const express = require('express')
+const userRoute = require('./routes/userRoute')
 const app = express()
 const port = 3000
 
+
+app.get('/', (req, res) => res.send('Olá, express'))
 app.listen(port, () => console.log('Api rodando na porta 3000'))
